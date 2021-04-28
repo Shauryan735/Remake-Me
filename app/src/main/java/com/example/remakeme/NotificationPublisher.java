3de3b5fabe1605aa93266c608275daec508eb8ff
JPKlein <jpklein@calpoly.edu>
2021-04-28 15:52:33 -0700

    NotificationPublisher added

--- app/src/main/java/com/example/remakeme/NotificationPublisher.java
+++ app/src/main/java/com/example/remakeme/NotificationPublisher.java
@@ -42,38 +42,43 @@ public class NotificationPublisher extends BroadcastReceiver {
         notificationManager.notify(m, notification);
     }
 
-    /**everything after this needs to go in the activity that will create the events**/
-    public void scheduleEventNotification(Event event){
-        assert(event.getSendReminders());
-        Notification notification = getEventNotification(event.getLocation(), event.getEventName(), event.getRemindOffset(),
-                event.getGroupColor(), event.getId());
+    public static void scheduleEventNotification(Context context, Event event){
+        assert(event.getSendReminders());//reminder for debugging
+        Notification notification = getEventNotification(
+                context,
+                event.getLocation(),
+                event.getEventName(),
+                event.getRemindOffset(),
+                event.getGroupColor(),
+                event.getId());
+
         Calendar now = Calendar.getInstance();
         long delay = event.getEventStart().getTimeInMillis() - now.getTimeInMillis() - event.getRemindOffset();
-        scheduleNotification(notification, delay, ((Integer)event.getId()).toString());
+        scheduleNotification(context, notification, delay, ((Integer)event.getId()).toString());
     }
 
-    private void scheduleNotification (Notification notification, long delay, String id) {
-        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+    private static void scheduleNotification (Context context, Notification notification, long delay, String id) {
+        Intent notificationIntent = new Intent(context, NotificationPublisher.class);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, id);
         notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
         /*TextView textView = findViewById(R.id.editNumNotifies);*/
-        PendingIntent pendingIntent = PendingIntent.getBroadcast (this, Integer.parseInt(id),
+        PendingIntent pendingIntent = PendingIntent.getBroadcast (context, Integer.parseInt(id),
                 notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager alarmManager = (AlarmManager)getSystemService/*just needs to be moved to correct context*/(Context.ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         assert alarmManager != null;
         long futureInMillis = SystemClock.elapsedRealtime() + delay;
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
     }
 
-    private Notification getEventNotification (String location, String eventName, long reminderOffset, int color, int id) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
+    private static Notification getEventNotification (Context context, String location, String eventName, long reminderOffset, int color, int id) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,
                 default_notification_channel_id);
         builder.setContentTitle(eventName);
         builder.setContentText("Starting in " + reminderOffset / 60000 + " minutes at " + location + ".");
         builder.setSmallIcon(R.drawable.ic_launcher_foreground);
         builder.setAutoCancel(true);
         builder.setChannelId(NOTIFICATION_CHANNEL_ID);
-        builder.setContentIntent(getEventNotificationIntent(eventName, id));
+        builder.setContentIntent(getEventNotificationIntent(context, eventName, id));
         if(color != 0) {
             builder.setColorized(true);
             builder.setColor(color);
@@ -81,16 +86,15 @@ public class NotificationPublisher extends BroadcastReceiver {
         return builder.build();
     }
 
-    private PendingIntent getEventNotificationIntent(String message, int id){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    private static PendingIntent getEventNotificationIntent(Context context, String message, int id){
+        Intent intent = new Intent(context, DisplayMessageActivity.class);
         /** NEED TO ADD THE CORRECT ACTIVITY CLASS HERE**/
 
 
         intent.putExtra(NotificationPublisher.getExtraMessage(), message);
         // Create the TaskStackBuilder and add the intent, which inflates the back stack
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
         stackBuilder.addNextIntentWithParentStack(intent);
-        PendingIntent pendingIntent = stackBuilder.getPendingIntent(id, 0);
-        return pendingIntent;
+        return stackBuilder.getPendingIntent(id, 0);
     }
 }
